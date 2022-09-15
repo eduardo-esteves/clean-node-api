49ce08c8a6b2defdf373612563fb5edfd55e6ade
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-09-15 19:55:59 -0300

    test: criado novo bloco de teste para validar se o password foi enviado no body da requisicao

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -19,7 +19,7 @@ describe('SignUp Controller', () => {
   test('Should return 400 if no email is provided', () => {
     const httpRequest = {
       body: {
-        name: 'any_email@mail.com',
+        name: 'Any Name',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
@@ -29,5 +29,20 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const httpRequest = {
+      body: {
+        name: 'Any Name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const sut = new SignUpController()
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 })
