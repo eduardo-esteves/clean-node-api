3b052ba30b2a45d7226a67d4008a3b13f1ba222a
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-09-15 20:01:20 -0300

    feature: adicionado novo campo obrigatorio a ser validado

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -3,7 +3,7 @@ import MisssingParamError from '../erros/MissingParamError'
 import { badRequest } from '../helpers/httpHelper'
 class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
