278d77933a260e7ec2d210ea288dfa1e12d46021
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-09-14 21:27:09 -0300

    Criado classe personalizada de erro extendendo de Error e substituido o uso do erro padrao Error pela classe personalizada

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,17 +1,18 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
+import MisssingParamError from '../erros/MissingParamError'
 class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name')
+        body: new MisssingParamError('name')
       }
     }
 
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: email')
+        body: new MisssingParamError('email')
       }
     }
   }
---
+++ src/presentation/erros/MissingParamError.ts
@@ -0,0 +1,8 @@
+class MisssingParamError extends Error {
+  constructor (paramName: string) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
+  }
+}
+
+export default MisssingParamError
